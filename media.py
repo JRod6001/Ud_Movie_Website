d22bfdfb5926d172e8e814935392caa8cf0e2a5e
JRod6001 <Jared.Terrore@gmail.com>
2016-11-21 20:49:22 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,26 +1,15 @@
 import webbrowser
 
 
-# class Video():
-#     
-#     def __init__(self, title, duration):
-#         self.title = title
-#         self.duration = duration
-
-
 class Movie():
-    """ This is some bullshit documentation for Movie()"""
-    VALID_RATINGS = ['G','PG','PG-13','R'] #each instance of this class has access to this constant
-    
+    """ This is some documentation for Movie()"""
+    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trialer_youtube):
-        # instance variables will be different across all classes 
-
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trialer_youtube
-        
-    def show_trailer(self): 
-        webbrowser.open(self.trailer_youtube_url)
-        
+
+    def show_trailer(self):
