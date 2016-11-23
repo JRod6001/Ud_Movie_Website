7a86381d952d5b3cebf9a6e4d859ccaa20d7a2c9
JRod6001 <Jared.Terrore@gmail.com>
2016-11-22 17:14:12 -0700

    Update media.py

--- media.py
+++ media.py
@@ -8,10 +8,12 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trialer_youtube):
+        # creates instance object containing movie information and content link
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trialer_youtube
 
     def show_trailer(self):
+        # opens movie trailer URL in a window
         webbrowser.open(self.trailer_youtube_url)
